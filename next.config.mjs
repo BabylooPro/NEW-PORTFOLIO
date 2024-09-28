cdf6f7c2f4f5553c255fa166dd299d6de011149f
BabylooPro <35376790+BabylooPro@users.noreply.github.com>
2024-09-28 03:34:50 +0200

    added: remove console log in production compiler

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
 	images: {
 		domains: ["avatars.githubusercontent.com", "cdnjs.cloudflare.com"],
 	},
+	compiler: {
+		removeConsole: {
+			exclude: ["error", "warn"],
+		},
+	},
 };
 
 export default nextConfig;
