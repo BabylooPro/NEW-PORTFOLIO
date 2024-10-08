71258f745acb2efea5fbce5fab4ce32b0217fce8
BabylooPro <babyloopro@gmail.com>
2024-10-08 15:51:13 +0200

    added: variable environnement access

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	env: {
+		GIT_HUB_ACCESS_TOKEN: process.env.GIT_HUB_ACCESS_TOKEN,
+		RESEND_API_KEY: process.env.RESEND_API_KEY,
+	},
 	images: {
 		domains: ["avatars.githubusercontent.com", "cdnjs.cloudflare.com"],
 	},
