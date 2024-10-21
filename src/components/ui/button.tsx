4f1eafb633cc7116fa647ad257120c0002705784
BabylooPro <babyloopro@gmail.com>
2024-10-21 06:16:32 +0200

    improved: variant handling and flexibility

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -8,6 +8,8 @@ const buttonVariants = cva(
 	{
 		variants: {
 			variant: {
+				// PRIMARY STYLE VARIANTS
+				none: "",
 				default: "bg-primary text-primary-foreground hover:bg-primary/90",
 				destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
 				outline:
@@ -15,14 +17,16 @@ const buttonVariants = cva(
 				secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
 				ghost: "hover:bg-accent hover:text-accent-foreground",
 				link: "text-primary underline-offset-4 hover:underline",
-				expandIcon: "group relative text-primary-foreground bg-primary hover:bg-primary/90",
+
+				// SECONDARY STYLE VARIANTS
+				expandIcon: "group relative",
 				ringHover:
-					"bg-primary text-primary-foreground transition-all duration-300 hover:bg-primary/90 hover:ring-2 hover:ring-primary/90 hover:ring-offset-2",
-				shine: "text-primary-foreground animate-shine bg-gradient-to-r from-primary via-primary/75 to-primary bg-[length:400%_100%] ",
+					"transition-all duration-300 hover:ring-2 hover:ring-primary hover:ring-offset-2",
+				shine: "animate-shine bg-gradient-to-r from-primary via-primary/75 to-primary bg-[length:400%_100%] ",
 				gooeyRight:
-					"text-primary-foreground relative bg-primary z-0 overflow-hidden transition-all duration-500 before:absolute before:inset-0 before:-z-10 before:translate-x-[150%] before:translate-y-[150%] before:scale-[2.5] before:rounded-[100%] before:bg-gradient-to-r from-zinc-400 before:transition-transform before:duration-1000  hover:before:translate-x-[0%] hover:before:translate-y-[0%] ",
+					"relative z-0 overflow-hidden transition-all duration-500 before:absolute before:inset-0 before:-z-10 before:translate-x-[150%] before:translate-y-[150%] before:scale-[2.5] before:rounded-[100%] before:bg-gradient-to-r from-zinc-400 before:transition-transform before:duration-1000  hover:before:translate-x-[0%] hover:before:translate-y-[0%] ",
 				gooeyLeft:
-					"text-primary-foreground relative bg-primary z-0 overflow-hidden transition-all duration-500 after:absolute after:inset-0 after:-z-10 after:translate-x-[-150%] after:translate-y-[150%] after:scale-[2.5] after:rounded-[100%] after:bg-gradient-to-l from-zinc-400 after:transition-transform after:duration-1000  hover:after:translate-x-[0%] hover:after:translate-y-[0%] ",
+					"relative z-0 overflow-hidden transition-all duration-500 after:absolute after:inset-0 after:-z-10 after:translate-x-[-150%] after:translate-y-[150%] after:scale-[2.5] after:rounded-[100%] after:bg-gradient-to-l from-zinc-400 after:transition-transform after:duration-1000  hover:after:translate-x-[0%] hover:after:translate-y-[0%] ",
 				linkHover1:
 					"relative after:absolute after:bg-primary after:bottom-2 after:h-[1px] after:w-full after:max-w-[80%] after:origin-bottom-left after:scale-x-100 hover:after:origin-bottom-right hover:after:scale-x-0 after:transition-transform after:ease-in-out after:duration-300",
 				linkHover2:
@@ -54,8 +58,9 @@ interface IconRefProps {
 
 export interface ButtonProps
 	extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-		VariantProps<typeof buttonVariants> {
+		Omit<VariantProps<typeof buttonVariants>, "variant"> {
 	asChild?: boolean;
+	variant?: string | string[];
 }
 
 export type ButtonIconProps = IconProps | IconRefProps;
@@ -63,8 +68,21 @@ export type ButtonIconProps = IconProps | IconRefProps;
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps & ButtonIconProps>(
 	({ className, variant, size, asChild = false, Icon, iconPlacement, ...props }, ref) => {
 		const Comp = asChild ? Slot : "button";
+		const variantClasses = Array.isArray(variant)
+			? cn(
+					...variant.map((v) =>
+						buttonVariants({
+							variant: v as VariantProps<typeof buttonVariants>["variant"],
+						})
+					)
+			  )
+			: buttonVariants({
+					variant: variant as VariantProps<typeof buttonVariants>["variant"],
+					size,
+			  });
+
 		return (
-			<Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props}>
+			<Comp className={cn(variantClasses, className)} ref={ref} {...props}>
 				{Icon && iconPlacement === "left" && (
 					<div className="w-0 translate-x-[0%] pr-0 opacity-0 transition-all duration-200 group-hover:w-5 group-hover:translate-x-100 group-hover:pr-2 group-hover:opacity-100">
 						<Icon />
