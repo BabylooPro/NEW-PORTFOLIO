7cb3433f91e0d48e303d9b55f22948f04054de5e
BabylooPro <babyloopro@gmail.com>
2024-10-21 06:28:27 +0200

    integrated: smooth animation transitions
    updated: styling for smoother transitions

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react";
 import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+import { motion, AnimatePresence } from "framer-motion";
 
 import { cn } from "@/lib/utils";
 
@@ -14,17 +15,28 @@ const TooltipTrigger = TooltipPrimitive.Trigger;
 const TooltipContent = React.forwardRef<
 	React.ElementRef<typeof TooltipPrimitive.Content>,
 	React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
->(({ className, sideOffset = 4, ...props }, ref) => (
-	<TooltipPrimitive.Content
-		ref={ref}
-		sideOffset={sideOffset}
-		className={cn(
-			"z-50 overflow-hidden rounded-md border bg-popover px-3 py-1.5 text-sm text-popover-foreground shadow-md animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-			className
-		)}
-		{...props}
-	/>
-));
+>(({ className, sideOffset = 4, children, ...props }, ref) => {
+	return (
+		<TooltipPrimitive.Content ref={ref} sideOffset={sideOffset} {...props} className="z-[99]">
+			<AnimatePresence>
+				<motion.div
+					className={cn(
+						"z-[99] overflow-hidden rounded-lg border bg-neutral-100 dark:bg-neutral-900 px-3.5 py-2 text-sm text-popover-foreground shadow-md",
+						"hidden md:block",
+						className
+					)}
+					initial={{ opacity: 0, scale: 0 }}
+					animate={{ opacity: 1, scale: 1, transition: { duration: 0.4 } }}
+					exit={{ opacity: 0, scale: 0, transition: { duration: 0.4 } }}
+					style={{ transformOrigin: "var(--radix-tooltip-content-transform-origin)" }}
+				>
+					{children}
+				</motion.div>
+			</AnimatePresence>
+		</TooltipPrimitive.Content>
+	);
+});
+
 TooltipContent.displayName = TooltipPrimitive.Content.displayName;
 
 export { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider };
