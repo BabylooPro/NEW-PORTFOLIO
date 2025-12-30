495eb281ec2ab8a9389d0133cd8b05d0297ec22c
BabylooPro <babyloopro@gmail.com>
2025-12-30 07:22:20 +0100

    disabled(temp) FeedbackRating and VisitorCounter component

--- frontend/src/features/landing/Footer.tsx
+++ frontend/src/features/landing/Footer.tsx
@@ -2,26 +2,26 @@
 
 import { CodeXml } from "lucide-react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+// import { usePathname } from "next/navigation";
 import React from "react";
 import { ShowInfo } from "@/components/ui/show-info";
-import { FeedbackRating } from "@/components/ui/feedback-badge";
-import { VisitorCounter } from "@/components/ui/visitor-counter";
+// import { FeedbackRating } from "@/components/ui/feedback-badge";
+// import { VisitorCounter } from "@/components/ui/visitor-counter";
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
-    const pathname = usePathname();
+    // const pathname = usePathname();
 
     return (
         <footer className="py-8 sm:py-12 md:py-16 flex flex-col justify-center items-center space-y-4">
-            <div className="flex flex-col items-center space-y-4 mb-32">
+            {/* <div className="flex flex-col items-center space-y-4 mb-8">
                 {pathname === "/" && (
                     <>
                         <FeedbackRating />
                         <VisitorCounter />
                     </>
                 )}
-            </div>
+            </div> */}
 
             <div className="flex items-center space-x-2 text-center">
                 <ShowInfo wrapMode>
